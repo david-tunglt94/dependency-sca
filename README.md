fb979a9ba6eb31c626dc1f6e3f97d77614e0c47b
david.tunglt <david.tunglt@gmail.com>
2022-08-28 18:46:41 +0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # dependency-sca
 dependency-sca is an open source tool performing a best effort analysis of 3rd party dependencies
+
+### lib
+List NVD (National Vulnerability Database - NVD) CVE (Common Vulnerabilities and Exposures - CVE)
+
+### reports
+Result scan dependency
+
+### docker
+```docker
+docker build -t tbdavid94/sca:tagname .
+docker push tbdavid94/sca:tagname
+docker pull tbdavid94/sca:tagname
+```
+
+### NOTE
+Unzip odc.mv.db =>lib/data...
---
+++ tests/PHP/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1ec354a5ee2d9b30411d5dd3632301908e9c8c4d',
+        'name' => 'dependency-sca/php',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dependency-sca/php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1ec354a5ee2d9b30411d5dd3632301908e9c8c4d',
+            'dev_requirement' => false,
+        ),
+        'laminas/laminas-escaper' => array(
+            'pretty_version' => '2.10.0',
+            'version' => '2.10.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laminas/laminas-escaper',
+            'aliases' => array(),
+            'reference' => '58af67282db37d24e584a837a94ee55b9c7552be',
+            'dev_requirement' => false,
+        ),
+        'phpoffice/phpword' => array(
+            'pretty_version' => '0.18.3',
+            'version' => '0.18.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpoffice/phpword',
+            'aliases' => array(),
+            'reference' => 'be0190cd5d8f95b4be08d5853b107aa4e352759a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
